163c4eebd34b4f4907171df3b56953467aef4676
Dave Ariens <dariens@blackberry.com>
2016-06-06 20:50:32 +0000

    added deleting pools, actions for expand/shrink, logic for determining missing names when expanding, view for confirming expansion, additional refactoring of vpool route defs to match verb patterns

--- app/views/vpool/__init__.py
+++ app/views/vpool/__init__.py
@@ -3,15 +3,16 @@ from flask import request, redirect, url_for, render_template, flash, Blueprint,
 from flask.ext.login import current_user, login_required
 from jinja2 import Environment
 from app import db
-from app.one import OneProxy
+from app.one import OneProxy, INCLUDING_DONE
 from app.views.template.models import ObjectLoader
-from app.views.vpool.models import PoolMembership, VirtualMachinePool, PoolTemplateForm, GenerateTemplateForm
+from app.views.vpool.models import PoolMembership, VirtualMachinePool, PoolEditForm, GenerateTemplateForm
 from app.views.common.models import ActionForm
 from app.views.zone.models import Zone
 from app.views.cluster.models import Cluster
 from app.views.template.models import VarParser
 from datetime import datetime
 import timeit
+import re
 
 
 vpool_bp = Blueprint('vpool_bp', __name__, template_folder='templates')
@@ -28,9 +29,87 @@ def test(pool_id):
   return render_template('vpool/test.html', pool=pool)
 
 
+@vpool_bp.route('/vpool/expand/<int:pool_id>', methods=['GET', 'POST'])
+@login_required
+def expand(pool_id):
+  pool = None
+  members = None
+  member_vms_by_num = {}
+  form = ActionForm()
+  try:
+    pool = VirtualMachinePool.query.get(pool_id)
+    members = pool.get_memberships()
+
+    start_time = timeit.default_timer()
+  except Exception as e:
+    flash("There was an error fetching pool_id={}: {}".format(pool_id, e), category='danger')
+  if (len(members) == pool.cardinality):
+    flash("Cannot expand {} ({}/{} members already exist)".format(pool.name, len(members), pool.cardinality))
+    return redirect(url_for('vpool.view', pool_id=pool.id))
+  if (len(members) > pool.cardinality):
+    flash("Cannot expand {} ({}/{} members, need to shrink)".format(pool.name, len(members), pool.cardinality))
+    return redirect(url_for('vpool.view', pool_id=pool.id))
+
+
+  member_vms_by_num = pool.get_member_vms_by_num()
+
+  for k, v in member_vms_by_num.items():
+    print("member: {} = {}".format(k, v.name))
+
+
+  new_names_by_num = {}
+  for number in range(1, pool.cardinality + 1):
+    if number not in member_vms_by_num:
+      new_name = pool.name_for_number(number)
+      new_names_by_num[int(number)] = new_name
+      print("number: {} not in member_vms_by_num, adding value: {}".format(number, new_name))
+
+  needed = pool.cardinality - len(member_vms_by_num)
+
+  if len(new_names_by_num) != needed:
+    for k, v in new_names_by_num.items():
+      print(k, v)
+    raise Exception("Error: needed {} new VMs but could only infer {} misssing names".format(needed, len(new_names_by_num)))
+
+  numbers = {}
+
+  for number in range(1, pool.cardinality + 1):
+    if number in new_names_by_num and number in member_vms_by_num:
+      raise Exception("VM number {} found in members and new names".format(number))
+    if number not in new_names_by_num and number not in member_vms_by_num:
+      raise Exception("VM number {} not found in members or new names".format(number))
+    numbers[number] = number
+
+
+  if request.method == 'POST' and form.validate():
+    try:
+      if request.form['action'] == 'cancel':
+        flash('Delete {} action cancelled'.format(pool.name), category='info')
+        return redirect(url_for('vpool_bp.view', pool_id=pool.id))
+      elif request.form['action'] == 'confirm':
+        redirect_url = url_for('cluster_bp.view', zone_number=pool.cluster.zone.number, cluster_id=pool.cluster.id)
+        members = pool.get_memberships()
+        for member in members:
+          db.session.delete(member)
+        db.session.delete(pool)
+        db.session.commit()
+        flash('Deleted pool {} with {} memberse'.format(pool.name, len(members)), category='success')
+        return redirect(url_for('cluster_bp.view', zone_number=pool.cluster.zone.number, cluster_id=pool.cluster.id))
+    except Exception as e:
+      #raise e
+      flash('There was an error deleting pool {}: {}'.format(pool.name, e), category='danger')
+      return redirect(url_for('vpool_bp.view', pool_id=pool.id))
+  return render_template('vpool/expand.html',
+                         form=form,
+                         pool=pool,
+                         numbers=numbers,
+                         member_vms_by_num=member_vms_by_num,
+                         new_names_by_num=new_names_by_num)
+
+
 @vpool_bp.route('/vpool/view/<int:pool_id>', methods=['GET', 'POST'])
 @login_required
-def view_pool(pool_id):
+def view(pool_id):
   pool = None
   vms_by_id = {}
   form = ActionForm()
@@ -38,11 +117,10 @@ def view_pool(pool_id):
     pool = VirtualMachinePool.query.get(pool_id)
     one_proxy = OneProxy(pool.cluster.zone.xmlrpc_uri, pool.cluster.zone.session_string, verify_certs=False)
     start_time = timeit.default_timer()
-    memberships = PoolMembership.query.filter_by(pool=pool)
-    for membership in memberships:
-      vms_by_id[membership.vm_id] = one_proxy.get_vm(membership.vm_id)
-    flash('fetched {} vms in {} time'.format(len(vms_by_id), start_time - timeit.default_timer()))
-
+    for vm in one_proxy.get_vms(INCLUDING_DONE):
+      if vm.disk_cluster is not None and vm.disk_cluster.id == pool.cluster.id:
+        vms_by_id[vm.id] = vm
+    flash('fetched {} vms in {} seconds'.format(len(vms_by_id), timeit.default_timer() - start_time))
   except Exception as e:
     traceback.print_exc(file=sys.stdout)
     flash("There was an error fetching pool_id={}: {}".format(pool_id, e), category='danger')
@@ -52,32 +130,70 @@ def view_pool(pool_id):
                          vms_by_id=vms_by_id)
 
 
-@vpool_bp.route('/vpool/<int:pool_id>/template', methods=['GET', 'POST'])
+@vpool_bp.route('/vpool/delete/<int:pool_id>', methods=['GET', 'POST'])
 @login_required
-def   edit_template(pool_id):
+def delete(pool_id):
+  pool = None
+  vms_by_id = {}
+  form = ActionForm()
+  try:
+    pool = VirtualMachinePool.query.get(pool_id)
+  except Exception as e:
+    flash("There was an error fetching pool_id={}: {}".format(pool_id, e), category='danger')
+  if request.method == 'POST' and form.validate():
+    try:
+      if request.form['action'] == 'cancel':
+        flash('Delete {} action cancelled'.format(pool.name), category='info')
+        return redirect(url_for('vpool_bp.view', pool_id=pool.id))
+      elif request.form['action'] == 'confirm':
+        redirect_url = url_for('cluster_bp.view', zone_number=pool.cluster.zone.number, cluster_id=pool.cluster.id)
+        members = pool.get_memberships()
+        for member in members:
+          db.session.delete(member)
+        db.session.delete(pool)
+        db.session.commit()
+        flash('Deleted pool {} with {} memberse'.format(pool.name, len(members)), category='success')
+        return redirect(url_for('cluster_bp.view', zone_number=pool.cluster.zone.number, cluster_id=pool.cluster.id))
+    except Exception as e:
+      #raise e
+      flash('There was an error deleting pool {}: {}'.format(pool.name, e), category='danger')
+      return redirect(url_for('vpool_bp.view', pool_id=pool.id))
+  return render_template('vpool/delete.html',
+                         form=form,
+                         pool=pool,
+                         vms_by_id=vms_by_id)
+
+
+@vpool_bp.route('/vpool/<int:pool_id>/edit', methods=['GET', 'POST'])
+@login_required
+def edit(pool_id):
   pool = VirtualMachinePool.query.get(pool_id)
-  form = PoolTemplateForm(request.form, obj=pool)
+  form = PoolEditForm(request.form, obj=pool)
   if request.method == 'POST':
     if request.form['action'] == "cancel":
       flash('Cancelled {} pool template update'.format(pool.name), category="info")
       return redirect(url_for('vpool_bp.view', pool_id=pool.id))
     elif request.form['action'] == "save":
       try:
+        cardinality_pattern = re.compile("\d+")
+        pool.name = request.form['name']
         pool.template = request.form['template']
         pool.vars = request.form['vars']
+        if not cardinality_pattern.fullmatch(request.form['cardinality']):
+          raise Exception("Cardinality {} not numeric".format(request.form['cardinality']))
+        pool.cardinality = request.form['cardinality']
         db.session.add(pool)
         db.session.commit()
         flash('Successfully saved pool template for {} (ID={}).'
               .format(pool.name, pool.id), 'success')
-        return redirect(url_for('vpool_bp.view_pool', pool_id=pool.id))
+        return redirect(url_for('vpool_bp.view', pool_id=pool.id))
       except Exception as e:
         flash('Failed to save pool template, error: {}'.format(e), 'danger')
   if form.errors:
     flash("Errors must be resolved before pool template can be saved", 'danger')
-  return render_template('vpool/template.html',
+  return render_template('vpool/edit.html',
                          form=form,
                          pool=pool)
-
 
 
 @vpool_bp.route('/vpool/<int:pool_id>/generate_template', methods=['GET', 'POST'])
@@ -86,7 +202,6 @@ def gen_template(pool_id):
   pool = VirtualMachinePool.query.get(pool_id)
   zone = Zone.query.get(pool.cluster.zone.number)
   cluster = Cluster.query.filter_by(zone=zone, id=pool.cluster.id).first()
-  pools = VirtualMachinePool.query.filter_by(cluster=cluster).all()
   form = GenerateTemplateForm(request.form)
   vars = {}
   var_string = None
@@ -115,11 +230,9 @@ def gen_template(pool_id):
                          pool=pool,
                          cluster=cluster,
                          form=form,
-                         pools=pools,
                          zone=zone,
                          var_string=var_string,
                          template=template)
-
 
 
 @vpool_bp.route('/assign_to_pool/zone/<int:zone_number>/cluster/<int:cluster_id>', methods=['GET', 'POST'])
@@ -136,20 +249,17 @@ def assign_to_pool(zone_number, cluster_id):
     zone = Zone.query.get(zone_number)
     cluster = Cluster.query.filter_by(zone=zone, id=cluster_id).first()
     one_proxy = OneProxy(zone.xmlrpc_uri, zone.session_string, verify_certs=False)
-
     for membership in PoolMembership.query.join(VirtualMachinePool).filter_by(cluster=cluster).all():
       memberships[membership.vm_id] = membership
-
-
     for vm in one_proxy.get_vms():
       if vm.disk_cluster.id == cluster.id:
         vms.append(vm)
         id_to_vm[vm.id] = vm
     pools = VirtualMachinePool.get_all(cluster)
   except Exception as e:
-    raise e
+    #raise e
     flash("Error fetching VMs in zone number {}: {}"
-          .format(zone_, e), category='danger')
+          .format(zone.number, e), category='danger')
   form = ActionForm()
   active_tab = 'create_new_pool'
   # Form submission handling begins
@@ -182,7 +292,10 @@ def assign_to_pool(zone_number, cluster_id):
         for vm_id in selected_vm_ids.keys():
           db.session.add(PoolMembership(pool=pool, vm_id=vm_id, date_added=datetime.utcnow()))
           db.session.commit()
-        flash('Added {} VMs to {}'.format(len(selected_vm_ids), pool.name))
+        flash(Markup('Successfully added {} members to pool <a href="{}">{}</a>'.format(
+          len(selected_vm_ids),
+          url_for('vpool_bp.view', pool_id=pool.id),
+          pool.name, )), category='success')
         return redirect(url_for('vpool_bp.assign_to_pool', zone_number=zone.number, cluster_id=cluster.id))
     if proceed and request.form['action'] == 'create new pool':
       try:
@@ -191,7 +304,8 @@ def assign_to_pool(zone_number, cluster_id):
         pool = VirtualMachinePool(
           name=request.form['new_pool_name'],
           cluster_id=cluster.id,
-          zone_number=zone.number)
+          zone_number=zone.number,
+          cardinality=len(selected_vm_ids))
         db.session.add(pool)
         db.session.flush()
         for vm_id in selected_vm_ids.keys():
@@ -201,7 +315,7 @@ def assign_to_pool(zone_number, cluster_id):
         db.session.flush()
         db.session.commit()
         flash(Markup('Successfully created <a href="{}">{}</a> with {} pool members'.format(
-          url_for('vpool_bp.view_pool', pool_id=pool.id),
+          url_for('vpool_bp.view', pool_id=pool.id),
           pool.name, len(selected_vm_ids))), category='success')
       except Exception as e:
         db.session.rollback()
--- app/views/vpool/models.py
+++ app/views/vpool/models.py
@@ -1,39 +1,71 @@
 from flask_wtf import Form
-from wtforms import TextAreaField
+from wtforms import TextAreaField, StringField
+from wtforms.validators import InputRequired
 from app.views.cluster.models import Cluster
 from app import db
-
+from app.one import OneProxy
+from app.one import INCLUDING_DONE
+import re
 
 class VirtualMachinePool(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(100), unique=True, nullable=False)
   cluster_id = db.Column(db.Integer,  nullable=False)
   zone_number = db.Column(db.Integer,  nullable=False)
-  template = db.Column(db.Text())
-  vars = db.Column(db.Text())
+  template = db.Column(db.Text(), default='{% extends cluster.template %}')
+  vars = db.Column(db.Text(), default='')
+  cardinality = db.Column(db.Integer, nullable=False, default=1)
   cluster = db.relationship(
     'Cluster',
     primaryjoin="and_(VirtualMachinePool.cluster_id == Cluster.id, VirtualMachinePool.zone_number == Cluster.zone_number)",
     foreign_keys=[cluster_id, zone_number])
 
-
-  def __init__(self, id=None, name=None, zone_number=None, cluster_id=None):
+  def __init__(self, id=None, name=None, zone_number=None, cluster_id=None, cardinality=None):
     self.id = id
     self.name = name
     self.cluster_id = cluster_id
     self.zone_number = zone_number
+    self.cardinality = cardinality
 
   def get_memberships(self):
-    return db.session.query(PoolMembership).join(
-      PoolMembership.pool, aliased=True).filter_by(id=self.id).all()
+    return PoolMembership.query.filter_by(pool=self).all()
+
+  def get_memberships_by_vm_id(self):
+    memberships = {}
+    for membership in self.get_memberships():
+      memberships[membership.vm_id] = membership
+    return memberships
+
+  def name_for_number(self, number):
+    pattern = re.compile("^([^\.]+)\.(.*)$")
+    match = pattern.match(self.name)
+    if match is None:
+      raise Exception("Failed to parse pool name for hostname of number: {}".format(number))
+    return '{}{}.{}'.format(match.group(1), number, match.group(2))
+
+
+  def get_member_vms_by_num(self):
+    num_pattern = re.compile("^[^\.]+(\d+)\.")
+    memberships_by_vm_id = self.get_memberships_by_vm_id()
+    vms_by_num = {}
+    one_proxy = OneProxy(self.cluster.zone.xmlrpc_uri, self.cluster.zone.session_string, verify_certs=False)
+    for vm in one_proxy.get_vms(INCLUDING_DONE):
+      if vm.id in memberships_by_vm_id:
+        match = num_pattern.match(vm.name)
+        if match is not None:
+          vms_by_num[int(match.group(1))] = vm
+    return vms_by_num
+
 
   def get_cluster(self):
     return Cluster.query.filter_by(zone_number=self.zone_number, id=self.cluster_id).first()
 
   @staticmethod
   def get_all(cluster):
-    return db.session.query(VirtualMachinePool).filter_by(cluster_id=cluster.id,
-                                                          zone_number=cluster.zone.number)
+    return db.session.query(VirtualMachinePool).filter_by(cluster=cluster)
+
+  def get_peer_pools(self):
+    return db.session.query(VirtualMachinePool).filter_by(cluster=self.cluster)
 
 
 class PoolMembership(db.Model):
@@ -54,7 +86,9 @@ class PoolMembership(db.Model):
       PoolMembership.pool, aliased=True).filter_by(zone=zone)
 
 
-class PoolTemplateForm(Form):
+class PoolEditForm(Form):
+  name = StringField('Name', [InputRequired()])
+  cardinality = StringField('Cardinality', [InputRequired()])
   template = TextAreaField('Zone Template')
   vars = TextAreaField('Zone Variables')
 
